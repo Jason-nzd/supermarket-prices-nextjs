3c78389a79cc088f04267569c16c7312bf1c1103
Jason Burgess <jason.nzd@gmail.com>
2023-02-23 21:39:53 +1300

    cleaned up next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // exportPathMap: async function(
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ){
-  //   return {
-
-  //   }
-  // },
-  // assetPrefix: './',
-  // trailingSlash: true,
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: false,
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'supermarketpricewatch.blob.core.windows.net',
-        port: '',
-        pathname: '**',
-      },
       {
         protocol: 'https',
         hostname: 'd1hhwouzawkav1.cloudfront.net',
