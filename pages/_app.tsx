a32b0023a04b1af59391f59bf241ff155a9b64fa
Jason Burgess <jason.nzd@gmail.com>
2023-03-14 19:31:38 +1300

    added title to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Manrope } from '@next/font/google';
+import Head from 'next/head';
 
 const manropeFont = Manrope({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={manropeFont.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>KiwiPrice.xyz</title>
+      </Head>
+      <main className={manropeFont.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
