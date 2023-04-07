8687c637d19e0d9572f3d64a49687de6d1cfad44
Jason Burgess <jason.nzd@gmail.com>
2023-04-07 16:10:48 +1200

    cleaned next.config.js

--- next.config.js
+++ next.config.js
@@ -13,20 +13,4 @@ module.exports = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,POST' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ];
-  },
 };
