3f662b59cabf5c9817c5ecdfb49671d24a5a7900
Jason Burgess <jason.nzd@gmail.com>
2023-04-13 18:32:20 +1200

    imagewithfallback now supports any relative src

--- components/ImageWithFallback.tsx
+++ components/ImageWithFallback.tsx
@@ -5,22 +5,21 @@ import fallbackImg from '../public/images/placeholder-square.png';
 interface Props {
   id: string;
   width: number;
-  addClasses?: string;
+  src: string;
 }
 
 // This is a modified replacement for <Image />
-// It will try to load a product image using 'id' as the filename,
+// It will try to load a product image using prop src,
 //  if nothing is found it will fallback to a placeholder image.
-function ImageWithFallback({ id, width = 200, addClasses = '' }: Props) {
+function ImageWithFallback({ id, width = 200, src }: Props) {
   const fallbackSrc = fallbackImg.src;
   const imagesHost = process.env.IMAGES_PATH || 'https://images.kiwiprice.xyz/';
-  const [imgSrc, setImgSrc] = useState(imagesHost + 'product-images/200/' + id + '.webp');
+  const [imgSrc, setImgSrc] = useState(imagesHost + src);
 
   return (
     <Image
       src={imgSrc}
       alt=''
-      className={addClasses}
       width={width}
       height={width}
       onError={() => {
