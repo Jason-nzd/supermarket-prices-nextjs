fa9987de92d21dcd8ab2477ae83625f64425475f
Jason Burgess <jason.nzd@gmail.com>
2023-04-17 20:57:56 +1200

    started pagination scrolling

--- components/ProductsGrid.tsx
+++ components/ProductsGrid.tsx
@@ -9,6 +9,7 @@ interface Props {
 }
 
 function ProductsGrid({ products, trimColumns = false }: Props) {
+  let numColumnsToShow = 3;
   let trimmedProducts: Product[] = [];
   if (trimColumns) {
     trimmedProducts = products;
@@ -19,35 +20,83 @@ function ProductsGrid({ products, trimColumns = false }: Props) {
     // Trim products to 4 columns
     if (useMediaQuery('1340px')) {
       trimmedProducts = products.slice(0, nextMultipleDown(products.length, 4));
+      numColumnsToShow = 4;
     }
     // Trim products to 5 columns
     if (useMediaQuery('2100px')) {
       trimmedProducts = products.slice(0, nextMultipleDown(products.length, 5));
+      numColumnsToShow = 5;
     }
   }
+
+  const [scrollIndex, setScrollIndex] = useState(0);
+  const [productsWindow, setProductsWindow] = useState<Product[]>(
+    products.slice(0, numColumnsToShow)
+  );
+
+  const scrollLeft = () => {
+    if (scrollIndex > 0) {
+      setScrollIndex(scrollIndex - 1);
+      const startIndex = (scrollIndex - 1) * numColumnsToShow;
+      const endIndex = scrollIndex * numColumnsToShow;
+
+      setProductsWindow(products.slice(startIndex, endIndex));
+      console.log('window - [' + startIndex + ' - ' + endIndex + ']');
+    }
+  };
+
+  const scrollRight = () => {
+    if (numColumnsToShow * scrollIndex < products.length) {
+      setScrollIndex(scrollIndex + 1);
+      const startIndex = scrollIndex * numColumnsToShow;
+      const endIndex = (scrollIndex + 1) * numColumnsToShow;
+
+      setProductsWindow(products.slice(startIndex, endIndex));
+      console.log('window - [' + startIndex + ' - ' + endIndex + ']');
+    }
+  };
+
   return (
-    <div
-      className='grid
+    <div className='flex items-center'>
+      {/* {products.length +
+        ' total products, window = ' +
+        productsWindow.length +
+        ' scroll = ' +
+        scrollIndex}
+      <button className='h-full border-2 rounded-xl p-2 hover:bg-white' onClick={scrollLeft}>
+        L
+      </button> */}
+      <div
+        className='grid
             grid-cols-1
             md:grid-cols-2
             lg:grid-cols-3
             xl:grid-cols-4
             2xl:grid-cols-4
             3xl:grid-cols-5'
-    >
-      {!trimColumns &&
-        products.map((product) => <ProductCard key={product.id} product={product} />)}
-      {trimColumns &&
-        trimmedProducts.map((product) => <ProductCard key={product.id} product={product} />)}
+      >
+        {!trimColumns &&
+          products.map((product) => <ProductCard key={product.id} product={product} />)}
+        {trimColumns &&
+          trimmedProducts.map((product) => <ProductCard key={product.id} product={product} />)}
+        {/* {productsWindow.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))} */}
+      </div>
+      {/* <button className='h-full border-2 rounded-xl p-2 hover:bg-white' onClick={scrollRight}>
+        R
+      </button> */}
     </div>
   );
 }
 
 export default ProductsGrid;
 
+// Get the final multiplier that fits within the range of inputNum
+// eg. inputNum = 45, multiplier = 6, returns 42 (7 multiples of 6)
 function nextMultipleDown(inputNum: number, multiplier: number): number {
-  let currentMultiple = 0;
-  let lastMultiple = 0;
+  let currentMultiple = multiplier;
+  let lastMultiple = multiplier;
   while (true) {
     currentMultiple += multiplier;
     if (currentMultiple === inputNum) return inputNum;
