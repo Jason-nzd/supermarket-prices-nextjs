623fdca474c7a979153e6817e636083484d56e25
Jason Burgess <jason.nzd@gmail.com>
2023-04-20 17:15:45 +1200

    tweaked star colours

--- components/StarFavourite.tsx
+++ components/StarFavourite.tsx
@@ -35,7 +35,7 @@ const starEmpty = (
     xmlns='http://www.w3.org/2000/svg'
     width='20'
     height='20'
-    fill='#007837'
+    fill='#86efac'
     className='bi bi-star'
     viewBox='0 0 16 16'
   >
