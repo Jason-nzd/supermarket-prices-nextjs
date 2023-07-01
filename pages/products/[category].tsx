d122be9433f3617881708e09e573ee97d482eeb6
Jason Burgess <jason.nzd@gmail.com>
2023-07-01 20:05:41 +1200

    fixed custom page static building

--- pages/products/[category].tsx
+++ pages/products/[category].tsx
@@ -148,7 +148,7 @@ let categoryNames = freshCategory.concat(
 
 // Remove special sub-categories witch have custom made pages instead of generated pages
 categoryNames = categoryNames.filter((name) => {
-  return !['eggs', 'fruit', 'milk', 'fresh-vegetables'].includes(name);
+  return !['eggs', 'fruit', 'milk', 'fresh-vegetables', 'spreads'].includes(name);
 });
 
 // Takes an array of categories, and returns them in { path } format
