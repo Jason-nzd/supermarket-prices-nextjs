3744327669c28a5ff00e4ee200806cd44b461f34
Jason Burgess <jason.nzd@gmail.com>
2023-07-05 16:00:10 +1200

    cleaned up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,22 +11,5 @@ module.exports = {
       '2xl': '1700px',
       '3xl': '2100px',
     },
-    // colors: {
-    //   blue: '#1fb6ff',
-    //   purple: '#7e5bef',
-    //   pink: '#ff49db',
-    //   orange: '#ff7849',
-    //   green: '#13ce66',
-    //   yellow: '#ffc82c',
-    //   'gray-dark': '#273444',
-    //   gray: '#8492a6',
-    //   'gray-light': '#d3dce6',
-    // },
-    // fontFamily: {
-    //   sans: ['Graphik', 'sans-serif'],
-    //   serif: ['Merriweather', 'serif'],
-    // },
-    extend: {},
   },
-  plugins: [],
 };
