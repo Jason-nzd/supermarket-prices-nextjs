6ae842e51088736e5472f37d68e2862971393394
Jason Burgess <jason.nzd@gmail.com>
2023-07-12 11:45:08 +1200

    cleanup

--- components/NavBar/CategorySelectMenu.tsx
+++ components/NavBar/CategorySelectMenu.tsx
@@ -1,5 +1,4 @@
 import { Dialog } from '@headlessui/react';
-
 import React, { useContext, useEffect, useState } from 'react';
 import SubCategoryList from './SubCategoryList';
 import { DarkModeContext } from '../../pages/_app';
