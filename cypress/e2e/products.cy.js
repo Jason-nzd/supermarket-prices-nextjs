af66e197fa65c20752d188180a668979d5faea95
Jason Burgess <jason.nzd@gmail.com>
2023-07-15 14:28:56 +1200

    added tests

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -1,6 +1,16 @@
 describe('Milk', () => {
-  it('should display product header', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000/products/milk');
-    cy.get('h1').contains('Milk');
+  });
+
+  it('should display website title, page header, products', () => {
+    cy.get('h1').contains('KiwiPrice.xyz');
+    cy.get('.grid-title').contains('Trending Products');
+    cy.get('.product-card').should('have.length.greaterThan', 3);
+  });
+
+  it('milk product should show title and dollar sign', () => {
+    cy.get('.product-card').first().find('div').contains('Milk');
+    cy.get('.product-card').first().find('#price').contains('$');
   });
 });
