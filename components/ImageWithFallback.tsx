59dbbc623e6e3d50eddce386c5be60eca103a29a
Jason Burgess <jason.nzd@gmail.com>
2023-08-12 14:04:02 +1200

    removed dark placeholder img

--- components/ImageWithFallback.tsx
+++ components/ImageWithFallback.tsx
@@ -1,8 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import fallbackImg from '../public/images/placeholder-square.png';
-import fallbackImgDark from '../public/images/placeholder-square-dark.png';
-import { DarkModeContext } from '../pages/_app';
 
 interface Props {
   id: string;
@@ -12,20 +10,20 @@ interface Props {
 
 // This is a modified replacement for <Image />
 // It will try to load a product image using prop src,
-//  if nothing is found it will fallback to a placeholder image.
+// if nothing is found it will fallback to a placeholder image.
 function ImageWithFallback({ width = 200, src }: Props) {
-  const fallbackSrc = useContext(DarkModeContext).darkMode ? fallbackImgDark.src : fallbackImg.src;
   const imagesHost = process.env.IMAGES_PATH || 'https://images.kiwiprice.xyz/';
   const [imgSrc, setImgSrc] = useState(imagesHost + src);
 
   return (
     <Image
       src={imgSrc}
+      className=''
       alt=''
       width={width}
       height={width}
       onError={() => {
-        setImgSrc(fallbackSrc);
+        setImgSrc(fallbackImg.src);
       }}
     />
   );
