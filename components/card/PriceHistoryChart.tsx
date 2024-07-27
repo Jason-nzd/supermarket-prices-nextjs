857b544d38a648f8c42fa52245da0c3f1c1705af
Jason B <jason.nzd@gmail.com>
2024-07-27 14:55:52 +1200

    changed price charts to fixed time scale and stepped line graphs

--- components/card/PriceHistoryChart.tsx
+++ components/card/PriceHistoryChart.tsx
@@ -1,117 +1,162 @@
-import React from 'react';
-import { DatedPrice } from '../../typings';
-import { priceTrend, PriceTrend, printPrice, utcDateToShortDate } from '../../utilities/utilities';
-import { CategoryScale, Chart, LinearScale, PointElement, LineElement, Tooltip } from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import type { ChartData, ChartOptions } from 'chart.js';
+import React from "react";
+import { DatedPrice } from "../../typings";
+import { priceTrend, PriceTrend, printPrice } from "../../utilities/utilities";
+import {
+  CategoryScale,
+  Chart,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeScale,
+  Tooltip,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import type { ChartData, ChartOptions } from "chart.js";
+import "chartjs-adapter-date-fns";
 
 interface Props {
   priceHistory: DatedPrice[];
   lastChecked: Date;
   useLargeVersion: boolean;
+  useSteppedLine?: boolean;
 }
 
-function PriceHistoryChart({ priceHistory, useLargeVersion = false, lastChecked }: Props) {
+function PriceHistoryChart({
+  priceHistory,
+  useLargeVersion = false,
+  useSteppedLine = true,
+}: Props) {
   // Initialize chart.js line chart
-  Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip);
+  Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    TimeScale,
+    Tooltip
+  );
 
-  // Separate arrays for x and y chart axis
-  let dateStringsOnly: string[] = [];
-  let priceDataOnly: number[] = [];
+  // Check if the price was updated today or not
+  const lastDatedPrice = priceHistory[priceHistory.length - 1];
+  const lastCheckedDateString = new Date(lastDatedPrice.date).toDateString();
+  const todayString = new Date().toDateString();
+  const wasUpdatedToday = lastCheckedDateString == todayString;
 
-  // Loop through each DatedPrice and remove day and year from date label
-  priceHistory.forEach((datedPrice) => {
-    dateStringsOnly.push(utcDateToShortDate(datedPrice.date, true));
-    priceDataOnly.push(datedPrice.price);
-  });
-
-  // Duplicate the most recent price point onto another price point when the product was last checked
-  //  this emphasises that the most recent price point is still valid when last checked
-  const mostRecentDate = new Date(priceHistory[priceHistory.length - 1].date).toDateString();
-  const today = new Date().toDateString();
-
-  if (mostRecentDate !== today) {
-    dateStringsOnly.push(utcDateToShortDate(lastChecked, true));
-    priceDataOnly.push(priceHistory[priceHistory.length - 1].price);
+  // If the price wasn't changed today, duplicate the most recent price point
+  // This highlights the last checked price should still be valid today
+  if (!wasUpdatedToday) {
+    let duplicatedDatedPrice: DatedPrice = {
+      date: new Date(),
+      price: priceHistory[priceHistory.length - 1].price,
+    };
+    priceHistory.push(duplicatedDatedPrice);
   }
 
   // Set line colour to green or red depending on price trend
-  let trendColour = '';
+  let trendColour = "";
   switch (priceTrend(priceHistory)) {
     case PriceTrend.Decreased:
-      trendColour = 'rgb(0, 255, 0)';
+      trendColour = "rgb(0, 255, 0)";
       break;
     case PriceTrend.Increased:
-      trendColour = 'rgb(255, 0, 0)';
+      trendColour = "rgb(255, 0, 0)";
       break;
+    case PriceTrend.Same:
     default:
-      trendColour = 'rgb(190, 190, 190)';
+      trendColour = "rgb(120, 120, 120)";
       break;
   }
 
   // Use smaller point radius for denser charts
-  let relativePointRadius = 3;
-  if (priceHistory.length > 6 && !useLargeVersion) relativePointRadius = 2;
+  let relativePointRadius = 1;
   if (priceHistory.length > 10 && !useLargeVersion) relativePointRadius = 0;
 
   // Prepare chart data for chart.js line chart
-  const chartData: ChartData<'line'> = {
-    labels: dateStringsOnly,
+  const chartData: ChartData<"line"> = {
+    labels: priceHistory.map((datedPrice) => datedPrice.date),
     datasets: [
       {
-        data: priceDataOnly,
+        data: priceHistory.map((datedPrice) => datedPrice.price),
         borderColor: trendColour,
         pointBorderColor: trendColour,
-        pointBackgroundColor: 'white',
+        pointBackgroundColor: "white",
         pointRadius: relativePointRadius,
         pointHoverRadius: 6,
         pointHitRadius: 30,
         pointBorderWidth: 3,
         borderWidth: 3,
-        tension: 0.2,
-        stepped: false,
+        tension: 0.1,
+        stepped: useSteppedLine ? "before" : false, // Toggle the stepped line graph
       },
     ],
   };
 
-  const options: ChartOptions<'line'> = {
+  const options: ChartOptions<"line"> = {
     responsive: true,
     maintainAspectRatio: false,
     resizeDelay: 200,
     plugins: {
       tooltip: {
         callbacks: {
+          // Tooltip title - set to blank to override default title
           title: () => {
-            return '';
+            return "";
           },
+
           label: function (context: any) {
-            return ' ' + printPrice(context.parsed.y);
+            // Tooltip body - show the date and price
+            const date = new Date();
+            date.setTime(context.parsed.x);
+            return (
+              date.toLocaleDateString("en-NZ", {
+                day: "numeric",
+                month: "short",
+                year: "2-digit",
+              }) +
+              "       " +
+              printPrice(context.parsed.y)
+            );
           },
         },
-        backgroundColor: 'white',
-        titleColor: 'black',
-        titleFont: { weight: 'normal' },
-        bodyColor: 'black',
-        footerColor: 'black',
+        backgroundColor: "white",
+        titleColor: "black",
+        titleFont: { weight: "normal" },
+        bodyColor: "black",
+        footerColor: "black",
         borderColor: trendColour,
         borderWidth: 1,
         padding: 10,
         cornerRadius: 14,
+        displayColors: false,
       },
     },
-    // scales: {
-    //   y: {
-    //     // min: 0,
-    //     ticks: {
-    //       callback(tickValue, index, ticks) {
-    //         return printPrice(tickValue as number);
-    //       },
-    //     },
-    //   },
-    // },
+    scales: {
+      x: {
+        type: "time",
+        time: {
+          unit: "month",
+          displayFormats: {
+            month: "MMM",
+            week: "dd MMM",
+          },
+        },
+      },
+      y: {
+        // suggestedMin: 0,
+        // suggestedMax: 100,
+      },
+      // y: {
+      //   // min: 0,
+      //   ticks: {
+      //     callback(tickValue, index, ticks) {
+      //       return printPrice(tickValue as number);
+      //     },
+      //   },
+      // },
+    },
   };
 
-  return <Line data={chartData} options={options} className='z-40' />;
+  return <Line data={chartData} options={options} className="z-40" />;
 }
 
 export default PriceHistoryChart;
