b36013428db10faf06de2b63f329485045b60b45
Jason B <jason.nzd@gmail.com>
2024-08-08 18:56:43 +1200

    wording tweak

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,32 +1,32 @@
-import { useContext } from 'react';
-import Footer from '../components/Footer';
-import NavBar from '../components/NavBar/NavBar';
-import { utcDateToMediumDate } from '../utilities/utilities';
-import { DarkModeContext } from './_app';
+import { useContext } from "react";
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar/NavBar";
+import { utcDateToMediumDate } from "../utilities/utilities";
+import { DarkModeContext } from "./_app";
 
 interface Props {
   lastChecked: string;
 }
 
 export default function Privacy({ lastChecked }: Props) {
-  const theme = useContext(DarkModeContext).darkMode ? 'dark' : 'light';
+  const theme = useContext(DarkModeContext).darkMode ? "dark" : "light";
 
   return (
     <main className={theme}>
       <NavBar lastUpdatedDate={lastChecked} />
       {/* Background Div */}
-      <div className='content-body'>
+      <div className="content-body">
         {/* Central Aligned Div */}
-        <div className='central-responsive-div'>
+        <div className="central-responsive-div">
           {/* Page Title */}
-          <div className='grid-title'>Privacy & Cookie Policy</div>
-          <div className='max-w-3xl mx-auto min-h-[20rem] px-2'>
+          <div className="grid-title">Privacy & Cookie Policy</div>
+          <div className="max-w-3xl mx-auto min-h-[20rem] px-2">
             <p>
-              Each user can choose favourite categories to appear at the top menu. These categories
-              are saved as a cookie with session expiry. The website can fully function if cookies
-              are disabled.
+              Each user can choose favourite categories to appear at the top
+              menu. These categories are saved as a cookie with session expiry.
+              The website can still fully function if cookies are disabled.
             </p>
-            <p className='mt-2'>No other information is collected on the user.</p>
+            <p className="mt-2">No other information is stored or collected.</p>
           </div>
         </div>
       </div>
