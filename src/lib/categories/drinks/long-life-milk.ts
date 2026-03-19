75f7426b0c1bde256a7db2b0bcd3bc402254fa59
Jason B <jason.nzd@gmail.com>
2026-03-19 22:13:45 +1300

    added long life milk sub categories

--- src/lib/categories/drinks/long-life-milk.ts
+++ src/lib/categories/drinks/long-life-milk.ts
@@ -1,6 +1,41 @@
-import { CategoryDefinitions } from "@/typings";
+import { CategoryDefinitions, SubCategory } from "@/typings";
+
+export function createSubCategory(sub: SubCategory): SubCategory {
+  return {
+    maxProductsToShow: 5,
+    titleAsSearchLink: true,
+    ...sub,
+  };
+}
 
 export const longLifeMilk: CategoryDefinitions["long-life-milk"] = {
   title: "Long Life & Plant Milk",
   icon: "🥛",
+  subcategories: [
+    createSubCategory({
+      titles: ["Long Life Milk"],
+      regexMatch: /^(?!.*(?:oat|soy|almond|coconut|plant)).*(?:uht).*/i,
+    }),
+    createSubCategory({
+      titles: ["Oat Milk"],
+      regexMatch: /oat/i,
+    }),
+    createSubCategory({
+      titles: ["Soy Milk"],
+      regexMatch: /soy/i,
+    }),
+    createSubCategory({
+      titles: ["Almond Milk"],
+      regexMatch: /almond/i,
+    }),
+    createSubCategory({
+      titles: ["Milk Powder"],
+      regexMatch: /powder/i,
+    }),
+  ],
+  otherSubcategory: {
+    useOtherSubcategory: true,
+    otherTitle: "Other",
+    otherMaxProductsToShow: 5,
+  },
 };
