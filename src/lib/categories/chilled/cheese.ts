d4cd7aad2afb617d6f6287978c7a575e787fb1cd
Jason B <jason.nzd@gmail.com>
2026-03-31 20:49:44 +1300

    added cheese and salad sub categories

--- src/lib/categories/chilled/cheese.ts
+++ src/lib/categories/chilled/cheese.ts
@@ -3,4 +3,51 @@ import { CategoryDefinitions } from "@/typings";
 export const cheese: CategoryDefinitions["cheese"] = {
   title: "Cheese",
   icon: "🧀",
+  subcategories: [
+    {
+      titles: ["Processed Cheese"],
+      regexMatch: /processed|chesdale|anchor.cheese.slices|laughing.cow/i,
+      maxProductsToShow: 5,
+    },
+    {
+      titles: ["Sliced Cheese"],
+      regexMatch: /slice/i,
+      maxProductsToShow: 5,
+    },
+    {
+      titles: ["Block Cheese", "Colby", "Edam", "Tasty", "Cheddar"],
+      regexMatch: /^(?!.*(grated|cottage)).*1kg|block|800g|colby|edam|tasty|cheddar/i,
+      maxProductsToShow: 5,
+      matchField: "both"
+    },
+    {
+      titles: ["Mozzarella", "Bocconcini", "Havarti", "Mild", "Ricotta", "Paneer"],
+      regexMatch: /^(?!.*(grated|cottage)).*mozzarella|bocconcini|havarti|mild|ricotta|paneer/i,
+      maxProductsToShow: 5,
+    },
+    {
+      titles: ["Camembert", "Brie", "Parmesan", "Feta", "Haloumi", "Blue Cheese", "Gruyere", "Gouda"],
+      regexMatch: /^(?!.*(grated)).*camembert|brie|parmesan|feta|hall?oumi|blue|gruyere|gouda/i,
+      maxProductsToShow: 5,
+    },
+    {
+      titles: ["Cottage Cheese"],
+      regexMatch: /cottage/i,
+      maxProductsToShow: 5,
+    },
+    {
+      titles: ["Cream Cheese"],
+      regexMatch: /cream.cheese/i,
+      maxProductsToShow: 5,
+    },
+    {
+      titles: ["Grated Cheese"],
+      regexMatch: /grated/i,
+      maxProductsToShow: 5,
+    },
+  ], otherSubcategory: {
+    useOtherSubcategory: true,
+    otherTitle: "Other",
+    otherMaxProductsToShow: 5,
+  },
 };
--- src/lib/categories/fresh/salads-coleslaw.ts
+++ src/lib/categories/fresh/salads-coleslaw.ts
@@ -3,4 +3,26 @@ import { CategoryDefinitions } from "@/typings";
 export const saladsColeslaw: CategoryDefinitions["salads-coleslaw"] = {
   title: "Salads & Coleslaw",
   icon: "🥗",
+  subcategories: [
+    {
+      titles: ["Coleslaw"],
+      regexMatch: /slaw/i,
+      maxProductsToShow: 10,
+    },
+    {
+      titles: ["Salads"],
+      regexMatch: /salad|baby|mix/i,
+      maxProductsToShow: 10,
+    },
+    {
+      titles: ["Sprouts"],
+      regexMatch: /sprout/i,
+      maxProductsToShow: 5,
+    },
+  ],
+  otherSubcategory: {
+    useOtherSubcategory: true,
+    otherTitle: "Other",
+    otherMaxProductsToShow: 10,
+  },
 };
